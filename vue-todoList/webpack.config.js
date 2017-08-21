5cbedec7aab6337126f79f7ce8c3a52e9c50b9c1
zhanghao <18408227355@163.com>
2017-08-21 21:57:27 +0800

    add modal

---
+++ vue-todoList/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
+  module: {
+    loaders:[
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+    ]
+  }
+}
